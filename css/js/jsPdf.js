a2e08b10648cf50cd62701f5316c96f51b013cb8
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-18 20:59:05 +0700

    a

--- css/js/jsPdf.js
+++
@@ -1,9 +0,0 @@
-import html2pdf from "../../node_modules/html2pdf.js";
-const generatePdfd = document.querySelector(".generatePdf");
-
-function generatePDF() {
-  const element = document.getElementById("content");
-  html2pdf(element);
-}
-
-generatePdfd.addEventListener("click", generatePDF);
